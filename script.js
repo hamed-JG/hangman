d15b78e675f6bd116617ba8580006e6747dcfeda
Hamed Jahan <iamhamedjahan@gmail.com>
2022-05-25 00:41:01 +0430

    writing script for running game

---
+++ script.js
@@ -0,0 +1,61 @@
+const secretWord = ["apply", "education", "high", "four", "client"];
+
+let randomItem = "";
+let clicked = [];
+let result = "";
+let mistakes = 0;
+function selectRandomItem() {
+  randomItem = secretWord[Math.floor(Math.random() * secretWord.length)];
+  document.getElementById("letters").addEventListener("click", buttonHandler);
+  console.log(randomItem);
+}
+
+function setUnderScores() {
+  let splitWord = randomItem.split("");
+  let mappedWord = splitWord.map((letter) =>
+    clicked.indexOf(letter) >= 0 ? letter : "_"
+  );
+  result = mappedWord.join("");
+  document.getElementById("clue").innerHTML = `<p>${result}</p>`;
+}
+
+function ifTrue() {
+  if (randomItem === result) {
+    document.getElementById("gameOver").querySelector("p").style.display =
+      "block";
+    document.getElementById("image").querySelector("img").src =
+      "assets/winner.png";
+  }
+}
+function ifFalse() {
+  if (mistakes === 6) {
+    document.getElementById("gameOver").querySelector("p").style.display =
+      "block";
+    document.getElementById(
+      "clue"
+    ).innerHTML = `<p>random word is ${randomItem}</p>`;
+  }
+}
+function hangManPic() {
+  const image = document.getElementById("image").querySelector("img");
+  image.src = `assets/hangman${mistakes}.png`;
+}
+function letterHandler(letter) {
+  letter = letter.toLowerCase();
+  clicked.indexOf(letter) === -1 ? clicked.push(letter) : null;
+  document.getElementById(letter.toUpperCase()).className = "used";
+  if (randomItem.indexOf(letter) >= 0) {
+    setUnderScores();
+    ifTrue();
+  } else if (randomItem.indexOf(letter) === -1) {
+    mistakes++;
+    ifFalse();
+    hangManPic();
+  }
+}
+function buttonHandler(event) {
+  letterHandler(event.target.id);
+}
+
+selectRandomItem();
+setUnderScores();
